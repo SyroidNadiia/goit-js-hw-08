0ade9b537f95bfb6621a34a50f88c561cc4c289d
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-02-15 21:01:15 +0200

    add task 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+const elementGalleryMakeup = createElementGallery(galleryItems);
+
+galleryRef.innerHTML = elementGalleryMakeup;
+
+function createElementGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
