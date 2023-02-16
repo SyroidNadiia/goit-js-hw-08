bfdff97759a7391d3734a2866dc7e39618d73be8
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-02-16 15:11:09 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const STORAGE_KEY = 'videoplayer-current-time';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+savedTime();
+
+player.on('timeupdate', throttle(onSaveCurrentTime, 1000));
+
+function onSaveCurrentTime(data) {
+  const currentTime = JSON.stringify(data.seconds);
+  localStorage.setItem(STORAGE_KEY, currentTime);
+}
+
+function savedTime() {
+  const savedCurrentTime = localStorage.getItem(STORAGE_KEY);
+
+  if (savedCurrentTime) {
+    const parseSavedCurrentTime = JSON.parse(savedCurrentTime);
+
+    player
+      .setCurrentTime(parseSavedCurrentTime)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            break;
+
+          default:
+            break;
+        }
+      });
+  }
+}
